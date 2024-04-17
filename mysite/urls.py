98b97cda60c25fc7cfd54c28cf06194655665b1b
salil <salil_siva@yahoo.com>
2024-04-17 10:45:11 +0100

    changed urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include
-from django.conf import urls
+from django.urls import path, include
+
 from django.contrib import admin
 
 urlpatterns = [
-    url('', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
 ]
